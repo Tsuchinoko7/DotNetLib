281df6b321fae997a92e368a0cedb4dcd4c13c02
tsuchinoko7 <tsuchinoko7.jp@gmail.com>
2015-07-20 18:53:49 +0900

    Equals追加

---
+++ CommonLib/CommonLib/t7/common/StringUtils.cs
@@ -0,0 +1,21 @@
+﻿namespace CommonLib
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class StringUtils
+    {
+        public static bool Equals(string val1, string val2)
+        {
+            if (val1 == null)
+            {
+                return val2 == null;
+            }
+
+            return val1.Equals(val2);
+        }
+    }
+}
---
+++ CommonLib/CommonLibTest/t7/common/StringUtilsTest.cs
@@ -0,0 +1,21 @@
+﻿namespace CommonLibTest
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using CommonLib;
+
+    [TestClass]
+    public class StringUtilsTest
+    {
+        [TestMethod]
+        public void TestEqual()
+        {
+            Assert.IsTrue(StringUtils.Equals(null, null));
+            Assert.IsTrue(StringUtils.Equals("a", "a"));
+            
+            Assert.IsFalse(StringUtils.Equals(null, "a"));
+            Assert.IsFalse(StringUtils.Equals("a", null));
+            Assert.IsFalse(StringUtils.Equals("a", "A"));
+        }
+    }
+}
