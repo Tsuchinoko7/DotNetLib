679ec6505a9166ce6b3ff0d1779e0b69e71a5b78
tsuchinoko7 <tsuchinoko7.jp@gmail.com>
2015-08-03 00:07:55 +0900

    NumberUtils ToInt IsInt

---
+++ CommonLib/CommonLib/t7/common/NumberUtils.cs
@@ -0,0 +1,19 @@
+﻿namespace t7.common
+{
+    public class NumberUtils
+    {
+        public static int ToInt(string val)
+        {
+            int result = 0;
+            bool isInt = int.TryParse(val, out result);
+
+            return result;
+        }
+
+        public static bool IsInt(string val)
+        {
+            int result = 0;
+            return int.TryParse(val, out result);
+        }
+    }
+}
---
+++ CommonLib/CommonLibTest/t7/common/NumberUtilsTests.cs
@@ -0,0 +1,28 @@
+﻿namespace CommonLib.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using t7.common;
+
+    [TestClass()]
+    public class NumberUtilsTests
+    {
+        [TestMethod()]
+        public void ToIntTest()
+        {
+            Assert.AreEqual(0, NumberUtils.ToInt("0"));
+            Assert.AreEqual(1, NumberUtils.ToInt("1"));
+            Assert.AreEqual(0, NumberUtils.ToInt(""));
+            Assert.AreEqual(0, NumberUtils.ToInt("a"));
+            Assert.AreEqual(-1, NumberUtils.ToInt("-1"));
+        }
+
+        [TestMethod()]
+        public void IsIntTest()
+        {
+            Assert.IsTrue(NumberUtils.IsInt("0"));
+            Assert.IsTrue(NumberUtils.IsInt("-1"));
+            Assert.IsFalse(NumberUtils.IsInt(""));
+            Assert.IsFalse(NumberUtils.IsInt("a"));
+        }
+    }
+}
