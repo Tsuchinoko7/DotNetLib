5172221b159d9666c2542c48bf23878cc91e9416
tsuchinoko7 <tsuchinoko7.jp@gmail.com>
2015-08-03 00:28:18 +0900

    ListBoxUtils TopScroll BottomScroll

---
+++ CommonLib/CommonLib/t7/form/ListBoxUtils.cs
@@ -0,0 +1,17 @@
+﻿namespace t7.common
+{
+    using System.Windows.Forms;
+
+    public class ListBoxUtils
+    {
+        public static void TopScroll(ListBox control)
+        {
+            control.TopIndex = 0;
+        }
+
+        public static void BottomScroll(ListBox control)
+        {
+            control.TopIndex = control.Items.Count - 1;
+        }
+    }
+}
