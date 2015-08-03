c894ac0e6bed8adab1f9caf3bda51f5d8be2aad5
tsuchinoko7 <tsuchinoko7.jp@gmail.com>
2015-08-04 07:50:54 +0900

    MessageBoxUtils

---
+++ CommonLib/CommonLib/t7/form/MessageBoxUtils.cs
@@ -0,0 +1,18 @@
+﻿namespace t7.common
+{
+    using Properties;
+    using System.Windows.Forms;
+
+    public class MessageBoxUtils
+    {
+        public static DialogResult Error(string message)
+        {
+            return MessageBox.Show(message, Resource.Error, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public static DialogResult Error(string message, string caption)
+        {
+            return MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
