ea9fccbde331962a77b5f6c3289e8ba7a03b9ab7
tsuchinoko7 <tsuchinoko7.jp@gmail.com>
2015-08-13 00:16:30 +0900

    リファクタリング

--- CommonLib/CommonLib/t7/common/StringUtils.cs
+++ CommonLib/CommonLib/t7/common/StringUtils.cs
@@ -1,42 +1,75 @@
 ﻿namespace t7.common
 {
+    /// <summary>
+    /// String Utils
+    /// </summary>
     public class StringUtils
     {
+        /// <summary>
+        /// A String for a space character.
+        /// </summary>
+        public const string SPACE = " ";
+
+        /// <summary>
+        /// The empty String <code>""</code>.
+        /// </summary>
+        public const string EMPTY = "";
+
+        /// <summary>
+        /// A String for linefeed LF ("\n").
+        /// </summary>
+        public const string LF = "\n";
+
+        /// <summary>
+        /// A String for carriage return CR ("\r").
+        /// </summary>
+        public const string CR = "\r";
+
+        /// <summary>
+        /// Represents a failed index search.
+        /// </summary>
         private const int INDEX_NOT_FOUND = -1;
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="val"></param>
-        /// <returns></returns>
-        public static bool IsEmpty(string val)
+        public StringUtils() : base()
         {
-            return val == null || val.Length == 0;
         }
 
         /// <summary>
-        /// 
+        /// <para>Checks if a String is empty ("") or null.</para>
         /// </summary>
-        /// <param name="val"></param>
-        /// <returns></returns>
-        public static bool IsNotEmpty(string val)
+        /// <param name="str">the String to check, may be null</param>
+        /// <returns>true if the String is empty or null</returns>
+        public static bool IsEmpty(string str)
         {
-            return !IsEmpty(val);
+            return str == null || str.Length == 0;
         }
 
         /// <summary>
-        /// 
+        /// <para>Checks if a String is not empty ("") and not null.</para>
         /// </summary>
-        /// <param name="vals"></param>
-        /// <returns></returns>
-        public static bool IsAnyEmpty(params string[] vals)
+        /// <param name="str">the String to check, may be null</param>
+        /// <returns>true if the String is not empty and not null</returns>
+        public static bool IsNotEmpty(string str)
         {
-            if (vals == null)
+            return !IsEmpty(str);
+        }
+
+        /// <summary>
+        /// <para>Checks if any one of the Strings are empty ("") or null.</para>
+        /// </summary>
+        /// <param name="strs">the Strings to check, may be null or empty</param>
+        /// <returns>true if any of the Strings are empty or null</returns>
+        public static bool IsAnyEmpty(params string[] strs)
+        {
+            if (strs == null)
             {
                 return true;
             }
 
-            foreach (var val in vals)
+            foreach (var val in strs)
             {
                 if (IsEmpty(val))
                 {
@@ -48,32 +81,32 @@
         }
 
         /// <summary>
-        /// 
+        /// <para>Checks if none of the Strings are empty ("") or null.</para>
         /// </summary>
-        /// <param name="vals"></param>
-        /// <returns></returns>
-        public static bool IsNoneEmpty(params string[] vals)
+        /// <param name="strs">the Strings to check, may be null or empty</param>
+        /// <returns>true if none of the Strings are empty or null</returns>
+        public static bool IsNoneEmpty(params string[] strs)
         {
-            return !IsAnyEmpty(vals);
+            return !IsAnyEmpty(strs);
         }
 
         /// <summary>
-        /// 
+        /// <para>Checks if a String is whitespace, empty ("") or null.</para>
         /// </summary>
-        /// <param name="val"></param>
-        /// <returns></returns>
-        public static bool IsBlank(string val)
+        /// <param name="str">the String to check, may be null</param>
+        /// <returns>true if the String is null, empty or whitespace</returns>
+        public static bool IsBlank(string str)
         {
-            if (val == null || val.Length == 0)
+            if (str == null || str.Length == 0)
             {
                 return true;
             }
 
-            int len = val.Length;
+            int len = str.Length;
 
             for (int i = (len - 1); i >= 0; i--)
             {
-                if (!char.IsWhiteSpace(val[i]))
+                if (!char.IsWhiteSpace(str[i]))
                 {
                     return false;
                 }
@@ -83,28 +116,28 @@
         }
 
         /// <summary>
-        /// 
+        /// <para>Checks if a String is not empty (""), not null and not whitespace only.</para>
         /// </summary>
-        /// <param name="val"></param>
-        /// <returns></returns>
-        public static bool IsNotBlank(string val)
+        /// <param name="str">the String to check, may be null</param>
+        /// <returns>true if the String is not empty and not null and not whitespace</returns>
+        public static bool IsNotBlank(string str)
         {
-            return !IsBlank(val);
+            return !IsBlank(str);
         }
 
         /// <summary>
-        /// 
+        /// <para>Checks if any one of the Strings are blank ("") or null and not whitespace only.</para>
         /// </summary>
-        /// <param name="vals"></param>
-        /// <returns></returns>
-        public static bool IsAnyBlank(params string[] vals)
+        /// <param name="strs">the Strings to check, may be null or empty</param>
+        /// <returns>true if any of the Strings are blank or null or whitespace only</returns>
+        public static bool IsAnyBlank(params string[] strs)
         {
-            if (vals == null)
+            if (strs == null)
             {
                 return true;
             }
 
-            foreach (var val in vals)
+            foreach (var val in strs)
             {
                 if (IsBlank(val))
                 {
@@ -116,33 +149,33 @@
         }
 
         /// <summary>
-        /// 
+        /// <para>Checks if none of the Strings are blank ("") or null and whitespace only.</para>
         /// </summary>
-        /// <param name="vals"></param>
-        /// <returns></returns>
-        public static bool IsNoneBlank(params string[] vals)
+        /// <param name="strs">the Strings to check, may be null or empty</param>
+        /// <returns>true if none of the Strings are blank or null or whitespace only</returns>
+        public static bool IsNoneBlank(params string[] strs)
         {
-            return !IsAnyBlank(vals);
+            return !IsAnyBlank(strs);
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="val"></param>
+        /// <param name="str"></param>
         /// <returns></returns>
-        public static string Trim(string val)
+        public static string Trim(string str)
         {
-            return val == null ? null : val.Trim();
+            return str == null ? null : str.Trim();
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="val"></param>
+        /// <param name="str"></param>
         /// <returns></returns>
-        public static string TrimToNull(string val)
+        public static string TrimToNull(string str)
         {
-            string result = Trim(val);
+            string result = Trim(str);
 
             return IsEmpty(result) ? null : result;
         }
@@ -150,151 +183,151 @@
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="val"></param>
+        /// <param name="str"></param>
         /// <returns></returns>
-        public static string TrimToEmpty(string val)
+        public static string TrimToEmpty(string str)
         {
-            return val == null ? string.Empty : Trim(val);
+            return str == null ? string.Empty : Trim(str);
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="val"></param>
+        /// <param name="str"></param>
         /// <returns></returns>
-        public static string Strip(string val)
+        public static string Strip(string str)
         {
-            return Trim(val);
+            return Trim(str);
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="val"></param>
+        /// <param name="str"></param>
         /// <returns></returns>
-        public static string StripToNull(string val)
+        public static string StripToNull(string str)
         {
-            return TrimToNull(val);
+            return TrimToNull(str);
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="val"></param>
+        /// <param name="str"></param>
         /// <returns></returns>
-        public static string StripToEmpty(string val)
+        public static string StripToEmpty(string str)
         {
-            return TrimToEmpty(val);
+            return TrimToEmpty(str);
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="val"></param>
+        /// <param name="str"></param>
         /// <param name="stripChars"></param>
         /// <returns></returns>
-        public static string Strip(string val, string stripChars)
+        public static string Strip(string str, string stripChars)
         {
-            string strip = StripStart(val, stripChars);
+            string strip = StripStart(str, stripChars);
             return StripEnd(strip, stripChars);
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="val"></param>
+        /// <param name="str"></param>
         /// <param name="stripChars"></param>
         /// <returns></returns>
-        public static string StripStart(string val, string stripChars)
+        public static string StripStart(string str, string stripChars)
         {
-            if (val == null || val.Length == 0)
+            if (str == null || str.Length == 0)
             {
-                return val;
+                return str;
             }
 
-            int len = val.Length;
+            int len = str.Length;
             int start = 0;
 
             if (stripChars == null)
             {
-                while (len > start && char.IsWhiteSpace(val[start]))
+                while (len > start && char.IsWhiteSpace(str[start]))
                 {
                     start++;
                 }
             }
             else
             {
-                while (len > start && stripChars.IndexOf(val[start]) != INDEX_NOT_FOUND)
+                while (len > start && stripChars.IndexOf(str[start]) != INDEX_NOT_FOUND)
                 {
                     start++;
                 }
             }
 
-            return val.Substring(start);
+            return str.Substring(start);
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="val"></param>
+        /// <param name="str"></param>
         /// <param name="stripChars"></param>
         /// <returns></returns>
-        public static string StripEnd(string val, string stripChars)
+        public static string StripEnd(string str, string stripChars)
         {
-            if (val == null || val.Length == 0)
+            if (str == null || str.Length == 0)
             {
-                return val;
+                return str;
             }
 
-            int end = val.Length;
+            int end = str.Length;
 
             if (stripChars == null)
             {
-                while (end > 0 && char.IsWhiteSpace(val[end - 1]))
+                while (end > 0 && char.IsWhiteSpace(str[end - 1]))
                 {
                     end--;
                 }
             }
             else
             {
-                while (end > 0 && stripChars.IndexOf(val[end - 1]) != INDEX_NOT_FOUND)
+                while (end > 0 && stripChars.IndexOf(str[end - 1]) != INDEX_NOT_FOUND)
                 {
                     end--;
                 }
             }
 
-            return val.Substring(0, end);
+            return str.Substring(0, end);
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="vals"></param>
+        /// <param name="strs"></param>
         /// <returns></returns>
-        public static string[] StripAll(string[] vals)
+        public static string[] StripAll(string[] strs)
         {
-            return StripAll(vals, null);
+            return StripAll(strs, null);
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="vals"></param>
+        /// <param name="strs"></param>
         /// <param name="stripChars"></param>
         /// <returns></returns>
-        public static string[] StripAll(string[] vals, string stripChars)
+        public static string[] StripAll(string[] strs, string stripChars)
         {
-            if (vals == null || vals.Length == 0)
+            if (strs == null || strs.Length == 0)
             {
-                return vals;
+                return strs;
             }
 
-            int valsLen = vals.Length;
-            string[] array = new string[vals.Length];
+            int valsLen = strs.Length;
+            string[] array = new string[strs.Length];
 
             for (int i = 0; i < valsLen; i++)
             {
-                array[i] = Strip(vals[i], stripChars);
+                array[i] = Strip(strs[i], stripChars);
             }
 
             return array;
@@ -303,109 +336,109 @@
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="val1"></param>
-        /// <param name="val2"></param>
+        /// <param name="str1"></param>
+        /// <param name="str2"></param>
         /// <returns></returns>
-        public static bool Equals(string val1, string val2)
+        public static bool Equals(string str1, string str2)
         {
-            if (val1 == null)
+            if (str1 == null)
             {
-                return val2 == null;
+                return str2 == null;
             }
 
-            return val1.Equals(val2);
+            return str1.Equals(str2);
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="val1"></param>
-        /// <param name="val2"></param>
+        /// <param name="str1"></param>
+        /// <param name="str2"></param>
         /// <returns></returns>
-        public static bool EqualsIgnoreCase(string val1, string val2)
+        public static bool EqualsIgnoreCase(string str1, string str2)
         {
-            if (val1 == null || val2 == null)
+            if (str1 == null || str2 == null)
             {
-                return val1 == val2;
+                return str1 == str2;
             }
 
-            return val1.ToLower().Equals(val2.ToLower());
+            return str1.ToLower().Equals(str2.ToLower());
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="val"></param>
+        /// <param name="str"></param>
         /// <param name="searchChar"></param>
         /// <returns></returns>
-        public static int IndexOf(string val, char searchChar)
+        public static int IndexOf(string str, char searchChar)
         {
-            if (IsEmpty(val))
+            if (IsEmpty(str))
             {
                 return INDEX_NOT_FOUND;
             }
 
-            return val.IndexOf(searchChar);
+            return str.IndexOf(searchChar);
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="val"></param>
+        /// <param name="str"></param>
         /// <param name="searchChar"></param>
         /// <param name="startPos"></param>
         /// <returns></returns>
-        public static int IndexOf(string val, char searchChar, int startPos)
+        public static int IndexOf(string str, char searchChar, int startPos)
         {
-            if (IsEmpty(val))
+            if (IsEmpty(str))
             {
                 return INDEX_NOT_FOUND;
             }
 
-            if (val.Length < startPos)
+            if (str.Length < startPos)
             {
                 return INDEX_NOT_FOUND;
             }
 
-            return val.IndexOf(searchChar, startPos < 0 ? 0 : startPos);
+            return str.IndexOf(searchChar, startPos < 0 ? 0 : startPos);
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="val"></param>
+        /// <param name="str"></param>
         /// <param name="searchString"></param>
         /// <returns></returns>
-        public static int IndexOf(string val, string searchString)
+        public static int IndexOf(string str, string searchString)
         {
-            if (val == null || searchString == null)
+            if (str == null || searchString == null)
             {
                 return INDEX_NOT_FOUND;
             }
 
-            if(IsEmpty(searchString))
+            if (IsEmpty(searchString))
             {
                 return 0;
             }
 
-            return val.IndexOf(searchString);
+            return str.IndexOf(searchString);
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="val"></param>
+        /// <param name="str"></param>
         /// <param name="searchString"></param>
         /// <param name="startPos"></param>
         /// <returns></returns>
-        public static int IndexOf(string val, string searchString, int startPos)
+        public static int IndexOf(string str, string searchString, int startPos)
         {
-            if (val == null || searchString == null)
+            if (str == null || searchString == null)
             {
                 return INDEX_NOT_FOUND;
             }
 
-            int len = val.Length;
+            int len = str.Length;
 
             if (IsEmpty(searchString))
             {
@@ -417,7 +450,7 @@
                 return INDEX_NOT_FOUND;
             }
 
-            return val.IndexOf(searchString, startPos < 0 ? 0 : startPos);
+            return str.IndexOf(searchString, startPos < 0 ? 0 : startPos);
         }
     }
 }
--- CommonLib/CommonLib/t7/form/ListBoxUtils.cs
+++ CommonLib/CommonLib/t7/form/ListBoxUtils.cs
@@ -2,13 +2,24 @@
 {
     using System.Windows.Forms;
 
+    /// <summary>
+    /// ListBox Control Utils
+    /// </summary>
     public class ListBoxUtils
     {
+        /// <summary>
+        /// Scroll to the top
+        /// </summary>
+        /// <param name="control"></param>
         public static void ScrollTop(ListBox control)
         {
             control.TopIndex = 0;
         }
 
+        /// <summary>
+        /// Scroll to the bottom
+        /// </summary>
+        /// <param name="control"></param>
         public static void ScrollBottom(ListBox control)
         {
             control.TopIndex = control.Items.Count - 1;
